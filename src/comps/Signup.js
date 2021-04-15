9f1625768c2e0f4ff6d4e08fc2520fc095198275
Pritam <pritamdebnath297@gmail.com>
2021-04-16 02:41:06 +0530

    feat : authentication from firebase
    created login page
    will make context page

---
+++ src/comps/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
+
+const Signup = () => {
+    return (
+        <Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log In
+                
+            </div>
+
+        </Card>
+    
+    )
+}
