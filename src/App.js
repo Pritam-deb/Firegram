1bb99b76b7947a5ffe0fda721f97dd5eaa519260
Pritam <pritamdebnath297@gmail.com>
2021-04-17 02:06:46 +0530

    Revert "feat : added routing"
    This reverts commit e81ab806face899757c8550ef39a2310bf077a0b.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-// import React, { useState } from 'react';
-// //import { Container } from 'react-bootstrap';
-// import ImageGrid from './comps/ImageGrid';
-// import Modal from './comps/Modal';
-// import SignIn from './comps/SignIn';
-// import Title from './comps/Title';
-// import UploadForm from './comps/UploadForm';
+import React, { useState } from 'react';
+import ImageGrid from './comps/ImageGrid';
+import Modal from './comps/Modal';
+import Title from './comps/Title';
+import UploadForm from './comps/UploadForm';
 
 
-// function App() {
-
-//   const [selectedImg, setSelectedImg] = useState(null);
-//   return (
-//     <div className="App">
-//       {/* <Container className=" d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}} >
-//         <div className="w-100" style={{ maxWidth: "400px" }} >
-//           <Signup></Signup>
-//         </div>
-        
-//       </Container> */}
-//       <Title/>
-//       <UploadForm/>
-//       <ImageGrid setSelectedImg={setSelectedImg} />
-//       {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import React from "react";
-import Application from "./comps/Application";
-import UserProvider from "./contexts/UserProvider";
 function App() {
+
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
-    <UserProvider>
-      <Application />
-    </UserProvider>
+    <div className="App">
+      <Title/>
+      <UploadForm/>
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
+    </div>
   );
 }
+
 export default App;
