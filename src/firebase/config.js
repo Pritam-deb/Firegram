4430b775c7422a220356cb7276ba5bae0eca6fd4
Pritam <pritamdebnath297@gmail.com>
2021-04-19 19:11:26 +0530

    fix: added auth function in firebase file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 
 import 'firebase/storage';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 
 // Your web app's Firebase configuration
@@ -19,5 +20,6 @@ firebase.initializeApp(firebaseConfig);
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const auth = firebase.auth();
 
-export { projectStorage, projectFirestore, timestamp };
+export { projectStorage, projectFirestore, timestamp, auth };
