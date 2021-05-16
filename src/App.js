e17843ce09de61cb5ecd5a873b52c18a46a5d588
Pritam <pritamdebnath297@gmail.com>
2021-05-16 23:49:27 +0530

    added new comp in App for login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
+import Signup from './comps/Signup';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm';
+import AuthProvider from './contexts/AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import Login from './comps/Login'
+import PrivateRoute from './comps/PrivateRoute';
+import ForgotPassword from './comps/ForgotPassword'
 
 
 
 function App() {
-
-  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
-      <Title/>
-      <UploadForm/>
-      <ImageGrid setSelectedImg={setSelectedImg} />
-      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
+      
+      <Container>
+        <div>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Home} />
+                {/* <Route exact path="/" component={Home} /> */}
+                <div className="d-flex align-items-center justify-content-center"
+                  style={{ minHeight: "100vh"}}>
+                <div className="w-100 " style={{ maxWidth: "400px"}}>
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+                </div>
+                </div>
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
     </div>
   );
 }
